be14a8bdab12b75378040527397f71c8f91891e0
Timothy Munene <munenegicharu@gmail.com>
2020-07-26 11:53:27 +0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design-image").slideToggle().hide();
+        $("#design h2").slideToggle();
+        $("#design p").slideToggle();
+    });
+
+    $("#design").click(function(){
+        $("#design h2").slideToggle().hide();
+        $("#design p").slideToggle().hide();
+        $("#design-image").slideToggle();
+    });
+
+    $("#development-image").click(function(){
+        $("#development-image").slideToggle().hide();
+        $("#development h2").slideToggle();
+        $("#development p").slideToggle();
+    });
+
+    $("#development").click(function(){
+        $("#development h2").slideToggle().hide();
+        $("#development p").slideToggle().hide();
+        $("#development-image").slideToggle();
+    });
+
+    $("#product-image").click(function(){
+        $("#product-image").slideToggle().hide();
+        $("#product h2").slideToggle();
+        $("#product p").slideToggle();
+    });
+
+    $("#product").click(function(){
+        $("#product h2").slideToggle().hide();
+        $("#product p").slideToggle().hide();
+        $("#product-image").slideToggle();
+    });
+});
